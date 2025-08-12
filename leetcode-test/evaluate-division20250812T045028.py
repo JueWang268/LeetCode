34980c2dfdd476da8c2588407cea0de0b437207b
Jue Wang <99457790+JueWang268@users.noreply.github.com>
2025-08-12 00:50:29 -0400

    Add solution for evaluate-division

---
+++ leetcode-test/evaluate-division20250812T045028.py
@@ -0,0 +1,33 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adj = defaultdict(dict)
+        ans = []
+        for i in range(len(equations)):
+            x, y = equations[i]
+            quotient = values[i]
+            adj[x][y] = quotient
+            adj[y][x] = 1/quotient
+        
+        def dfs(product, node, target, seen):
+            # float, str, str, set
+            if node not in adj or target not in adj or node in seen:
+                return 0
+            if target == node:
+                return product
+            
+            seen.add(node)
+            for neighbor in adj[node]:
+                res = dfs(product*adj[node][neighbor], neighbor, target, seen)
+                if res:
+                    return res
+            seen.remove(node)
+            return 0
+
+        for v1, v2 in queries:
+            q = dfs(1, v1, v2, set())
+            if q:
+                ans.append(q)
+                continue
+            ans.append(-1)
+        return ans
+
