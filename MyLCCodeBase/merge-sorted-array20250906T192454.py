2c478befcec68622531920262f89c86dc736b592
Jue Wang <99457790+JueWang268@users.noreply.github.com>
2025-09-06 15:24:55 -0400

    Add solution for merge-sorted-array

---
+++ MyLCCodeBase/merge-sorted-array20250906T192454.py
@@ -0,0 +1,24 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # go backward
+        onepos =  m - 1
+        twopos = n - 1
+        ptr = len(nums1) - 1
+        while onepos > -1 and twopos > -1:
+            if nums1[onepos] > nums2[twopos]:
+                nums1[ptr] = nums1[onepos]
+                onepos -= 1
+                ptr -= 1
+                continue
+            nums1[ptr] = nums2[twopos]
+            twopos -= 1
+            ptr -= 1
+        # put the rest of nums2 in nums1
+        while twopos > -1 and ptr > -1:
+            nums1[ptr] = nums2[twopos]
+            ptr -= 1
+            twopos -= 1
+        return
